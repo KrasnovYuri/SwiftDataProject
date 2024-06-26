f8079cbc96cf7e6a02bfb9d8289bccff8f04eada
KrasnovYuri <krasnov1yuri@gmail.com>
2024-06-26 21:55:35 +0300

    Start project

---
+++ SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Юрий on 26.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
