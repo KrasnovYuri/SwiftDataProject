c5fcd91f02358856b3a16d6f4216cbf8b4d2e736
KrasnovYuri <krasnov1yuri@gmail.com>
2024-07-21 13:42:46 +0300

    Editing SwiftData model objects

--- SwiftDataProject/SwiftDataProjectApp.swift
+++ SwiftDataProject/SwiftDataProjectApp.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Юрий on 26.06.2024.
 //
-
+import SwiftData
 import SwiftUI
 
 @main
@@ -13,5 +13,6 @@ struct SwiftDataProjectApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: User.self)
     }
 }
